b55ed9096ff77e7e1e6529b7a90ba4e0e8a78771
Dan Stagg <danstagg@gmail.com>
2023-10-22 11:35:03 +0100

    minor modifications

--- translator.py
+++ translator.py
@@ -1,6 +1,6 @@
 from translate import Translator
 
-print("ENGLISH TO GERMAN TRANSLATOR")
+print("ENGLISH TRANSLATOR")
 language_sel = ""
 language = input("\n1 - German\n2 - French\nSelect a language: ")
 if language == "1":
@@ -11,8 +11,8 @@ else:
 translator = Translator(to_lang=language_sel)
 
 while True:
-	user_text = input("Enter text to translate 'q' to quit: ")
+	user_text = input("\nEnter text to translate 'q' to quit:\n")
 	if user_text.lower() == "q":
 		break
 	translation = translator.translate(user_text)
-	print(f"\nYour translation:\n: {translation}")
+	print(f"\nYour translation:\n{translation}")
